e07e825c94bf6d6c1f796b0c87cbf4be34de19b0
Tomasz Staniewicz <tomasz.staniewicz.external@atos.net>
2020-06-15 10:14:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## gRPC proto validate generator
+
+* `mvn compile`
+* Use `HelloWorldServerWithInterceptor` Which is more idiomatic gRPC Way which works with gRPC Interceptors 
---
+++ src/test/java/my/proto/Tests.java
@@ -0,0 +1,28 @@
+package my.proto;
+
+import com.google.protobuf.Message;
+import io.envoyproxy.pgv.ReflectiveValidatorIndex;
+import io.envoyproxy.pgv.ValidationException;
+import io.envoyproxy.pgv.ValidatorIndex;
+import io.grpc.examples.helloworld.HelloRequest;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class Tests {
+
+    @Test
+    public void testIfValidationWorks() throws ValidationException {
+        HelloRequest message = HelloRequest.newBuilder()
+            .addAllField1(Arrays.asList("alama", "makota"))
+            .build();
+
+        validate(message);
+    }
+
+    private static void validate(Message message) throws ValidationException {
+        ValidatorIndex index = new ReflectiveValidatorIndex();
+        // Assert that a message is valid
+        index.validatorFor(message.getClass()).assertValid(message);
+    }
+
+}
